739a0ee5f87c766bfdcda98b6a1952646c9cf054
hammett <hammett@gmail.com>
2015-03-17 14:39:52 +1300

    basic message/handler api

---
+++ src/Castle.RabbitMq/Extensions/api.cs
@@ -0,0 +1,34 @@
+﻿namespace Castle.RabbitMq.Extensions
+{
+    using System;
+
+    public interface IMessage
+    {
+    }
+
+    public interface IMessageHandler<in TMessage> where TMessage : IMessage
+    {
+        void Handle(TMessage message);
+    }
+
+    public interface IRoutable : IMessage
+    {
+        string RoutingKey { get; }
+    }
+
+    [Serializable]
+    public class MessagingScopeAttribute : Attribute
+    {
+        public MessagingScopeAttribute(string scope)
+        {
+            this.Scope = scope;
+        }
+
+        public string Scope { get; private set; }
+
+        public string[] GetScopes()
+        {
+            return this.Scope.Split(',');
+        }
+    }
+}
