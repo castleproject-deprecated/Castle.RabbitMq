ee95cb6c6ac2ca14f84697adb7af5a69160b1814
hammett <hammett@gmail.com>
2015-04-15 19:24:36 -0700

    implemented protobuf serializer

--- src/Castle.RabbitMq/Serializers/ProtoBufSerializer.cs
+++ src/Castle.RabbitMq/Serializers/ProtoBufSerializer.cs
@@ -1,23 +1,44 @@
 ﻿namespace Castle.RabbitMq.Serializers
 {
     using System;
+    using System.IO;
+    using ProtoBuf.Meta;
     using RabbitMQ.Client;
 
 	public class ProtoBufSerializer : IRabbitSerializer
     {
-		public byte[] Serialize<T>(T instance, IBasicProperties prop)
-        {
-            throw new NotImplementedException();
-        }
+		private readonly RuntimeTypeModel _rtModel;
 
-		public T Deserialize<T>(byte[] data, IBasicProperties prop)
-        {
-            throw new NotImplementedException();
-        }
+		public ProtoBufSerializer() : this(RuntimeTypeModel.Default)
+		{
+		}
+
+		public ProtoBufSerializer(RuntimeTypeModel rtModel)
+		{
+			_rtModel = rtModel;
+		}
+
+		public byte[] Serialize<T>(T instance, IBasicProperties prop)
+		{
+			var memoryStream = new MemoryStream();
+			_rtModel.Serialize(memoryStream, instance);
+			var buffer = memoryStream.GetBuffer();
+			var length = (int) memoryStream.Length;
+			if (buffer.Length == length) return buffer;
+
+			var data = new byte[length];
+			Buffer.BlockCopy(buffer, 0, data, 0, length);
+			return data;
+		}
 
 		public object Deserialize(byte[] data, Type type, IBasicProperties prop)
         {
-            throw new NotImplementedException();
+			return _rtModel.Deserialize(new MemoryStream(data), null, type);
         }
-    }
+
+		public T Deserialize<T>(byte[] data, IBasicProperties prop)
+		{
+			return (T) Deserialize(data, typeof(T), prop);
+		}
+	}
 }
