278337ae478a7e2c6892b0738099170ce1af9d4e
hammett <hammett@gmail.com>
2015-06-08 17:56:05 -0700

    symmetry is a good quality to expose

--- src/Castle.RabbitMq/IRabbitChannel.cs
+++ src/Castle.RabbitMq/IRabbitChannel.cs
@@ -29,11 +29,18 @@
 
 	public static class	RabbitChannelExtensions
 	{
-		public static IRabbitQueueBinding Bind(this	IRabbitChannel source, 
+		public static IRabbitQueueBinding Bind(this IRabbitChannel source, 
 											   string exchange,	string queue,
 											   string routingKeyOrFilter)
 		{
 			return (source as RabbitChannel).BindInternal(false, queue, exchange, routingKeyOrFilter);
+		}
+
+		public static IRabbitQueueBinding BindNoWait(this IRabbitChannel source,
+											   string exchange, string queue,
+											   string routingKeyOrFilter)
+		{
+			return (source as RabbitChannel).BindInternal(true, queue, exchange, routingKeyOrFilter);
 		}
 
 		public static IRabbitExchange DeclareExchange(this IRabbitChannel source, string name, RabbitExchangeType exchangeType)
