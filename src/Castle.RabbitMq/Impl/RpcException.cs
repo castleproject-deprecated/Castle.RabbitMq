1fd2baebdbc8aa23fbe8c85c43eb0b3d64fa737c
hammett <hammett@gmail.com>
2015-06-15 21:02:43 -0700

    introduced exception for rpc calls

---
+++ src/Castle.RabbitMq/Impl/RpcException.cs
@@ -0,0 +1,21 @@
+﻿namespace Castle.RabbitMq
+{
+	using System;
+	using System.Runtime.Serialization;
+
+	[Serializable]
+	public class RpcException : Exception
+	{
+		public RpcException(string message) : base(message)
+		{
+		}
+
+		public RpcException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		protected RpcException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
