5d37b0676a5afb04df38126db06a3285d791ab58
hammett <hammett@gmail.com>
2015-07-01 18:53:36 -0700

    removed unused

--- src/extensions/Castle.RabbitMq.WindsorIntegration.Tests/MessagesAndHandlers/MyRoutableMessage.cs
+++
@@ -1,9 +0,0 @@
-namespace Castle.RabbitMq.WindsorIntegration.Tests
-{
-    using Castle.RabbitMq.Messaging;
-
-    public class MyRoutableMessage : IMessage, IRoutable
-    {
-        public string RoutingKey { get; set; }
-    }
-}
